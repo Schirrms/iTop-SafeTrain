41b2a15195aafe368494d4479b74be0a04410534
Schirrms <schirrms@schirrms.net>
2023-11-24 18:31:50 +0100

    💄 Some GUI cleaning

--- schirrms-safetrain/fr.dict.schirrms-safetrain.php
+++ schirrms-safetrain/fr.dict.schirrms-safetrain.php
@@ -10,7 +10,7 @@
 	// Dictionary entries go here
 	'Class:FunctionalCI/Attribute:s_train' => 'Membre du/des Train(s) SAFe',
 	'Class:SafeTrain' => 'Train SAFe',
-  'Class:SafeTrain/Attribute:name' => 'nom du train SAFe',
+  'Class:SafeTrain/Attribute:name' => 'Nom du train SAFe',
   'Class:SafeTrain/Attribute:name+' => 'Devrait être le même nom que le nom du train dans les étiquettes',
   'Class:SafeTrain/Attribute:s_train' => 'Code de l\'étiquette associé à ce train SAFe',
   'Class:SafeTrain/Attribute:s_train+' => 'DOIT être une étiquette valide !',
