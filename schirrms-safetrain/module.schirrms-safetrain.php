eaebab4f46b966892798d641e5b99e0ec69d1562
Schirrms <schirrms@schirrms.net>
2026-01-26 16:59:01 +0100

    🐛wrong def while converting to 3.0 format

--- schirrms-safetrain/module.schirrms-safetrain.php
+++ schirrms-safetrain/module.schirrms-safetrain.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'schirrms-safetrain/0.5.0',
+	'schirrms-safetrain/0.5.1',
 	array(
 		// Identification
 		//
